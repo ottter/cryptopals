7abfc2e8002527148cc43fe9367e1caaeba84395
ottter <jamescross808@gmail.com>
2020-01-02 15:12:43 -0500

    set 1, challenge 4

--- set-1.py
+++ set-1.py
@@ -1,5 +1,5 @@
 import codecs
-import string
+import os
 
 # Challenges from https://cryptopals.com/sets/1
 
@@ -47,21 +47,36 @@ def single_byte_xor(input_value):
 
     results = (sorted(results, key=lambda c: c['score'], reverse=True))
     # Outputs the top 5 results. Before adding space as a value, the correct answer was 5th
-    for result in list(results)[0:5]:
+    for result in list(results)[0:1]:
+        # print(f'Score:{result["score"]}\t Output: {result["result"]}')
+        return result
+
+def single_byte_xor_2():
+    """Set 1, Challenge 4
+       One of the 60-character strings in set1-chal4.txt has been encrypted by single-character XOR."""
+    # Trying to not use functions was an awful idea. being stubborn about it was worse
+    results = []
+    input_str = [line.strip() for line in open('set1-chal4.txt', 'r')]
+    for result in input_str:
+        output = single_byte_xor(result)
+        results.append(output)
+    results = (sorted(results, key=lambda c: c['score'], reverse=True))
+    for result in list(results)[0:1]:
         print(f'Score:{result["score"]}\t Output: {result["result"]}')
 
+### To try yourself: uncomment the function call below and run
 
 ### Set 1, Challenge 1 https://cryptopals.com/sets/1/challenges/1
-# hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
+hex_to_base64('49276d206b696c6c696e6720796f757220627261696e206c696b65206120706f69736f6e6f7573206d757368726f6f6d')
 
 ### Set 1, Challenge 2 https://cryptopals.com/sets/1/challenges/2
-# fixed_xor('1c0111001f010100061a024b53535009181c')
+fixed_xor('1c0111001f010100061a024b53535009181c')
 
 ### Set 1, Challenge 3
-single_byte_xor("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736")
+print(single_byte_xor("1b37373331363f78151b7f2b783431333d78397828372d363c78373e783a393b3736"))
+# This one is kind of ugly because I wanted to do it all in one function
 
 ### Set 1, Challenge 4
-
+single_byte_xor_2()
 
 ### Set 1, Challenge 5
-
