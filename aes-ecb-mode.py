eca9a0e4db11e472c704a65558a75c7882bfbf73
ottter <jamescross808@gmail.com>
2020-01-04 14:45:23 -0500

    set 1, challenge 7

---
+++ aes-ecb-mode.py
@@ -0,0 +1,26 @@
+"""
+Set 1, Challenge 7
+The Base64-encoded content in this file has been encrypted via AES-128 in ECB mode under the key
+"YELLOW SUBMARINE".
+(case-sensitive, without the quotes; exactly 16 characters; I like "YELLOW SUBMARINE" because it's exactly 16 bytes long, and now you do too).
+Decrypt it. You know the key, after all.
+Easiest way: use OpenSSL::Cipher and give it AES-128-ECB as the cipher.
+"""
+
+from base64 import b64decode
+from Crypto.Cipher import AES
+
+KEY = b'YELLOW SUBMARINE'
+
+def aes_ecb_decryption(cipher_text, key):
+    cipher = AES.new(key, AES.MODE_ECB)
+    data = cipher.decrypt(cipher_text)
+    return data
+
+def main():
+    with open('aes-ecb-mode-ciphertext.txt', 'rb') as input_file:
+        cipher_text = b64decode(input_file.read())
+    print(f'key: {KEY.decode()}\nplain_text: \n{aes_ecb_decryption(cipher_text, KEY).decode()}')
+
+if __name__ == "__main__":
+    main()
